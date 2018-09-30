f90643e1ceac5286ace740e1bba46c05a4ac6d49
eelcohn <eelco@huininga.nl>
2018-09-30 14:55:02 +0200

    Added check for HTTP header Via

--- WSA.ps1
+++ WSA.ps1
@@ -17,7 +17,7 @@ $TimeOut					= 10
 $UseProxy					= $true
 
 # Global system variables
-$WSAVersion					= "v20180920"
+$WSAVersion					= "v20180930"
 $Protocols					= @("https")
 $SSLLabsAPIUrl				= "https://api.ssllabs.com/api/v3/analyze"
 $SecurityHeadersAPIUrl		= "https://securityheaders.com/"
